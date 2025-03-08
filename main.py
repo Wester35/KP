a62218f1fe8da1437f6624f0d9e32afd9302e129
Wester35 <shum0302@gmail.com>
2025-03-08 23:59:45 +0300

    create background on login form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
 from ui.ui_login import Ui_MainWindow  # Импортируем сгенерированный класс
 from PySide6.QtGui import QPixmap
 
@@ -10,10 +10,20 @@ class LoginApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+
+
         #Подключение кнопок
         self.ui.pushButton.clicked.connect(self.handle_login)
 
         self.ui.toolButton.setIcon(QPixmap("ui/resources/free-icon-login-1674704.png"))
+        # Создаём QLabel для фона
+        self.background = QLabel(self.ui.frame_2)
+        self.background.setGeometry(0, 0, 600, 600)
+
+        # Загружаем изображение как фон
+        pixmap = QPixmap("ui/resources/background.jpg")  # Укажи свой путь
+        self.background.setPixmap(pixmap)
+        self.background.setScaledContents(True)
 
     def handle_login(self):
         pass
--- ui/ui_login.py
+++ ui/ui_login.py
@@ -15,52 +15,77 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QStatusBar, QToolButton,
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QToolButton,
     QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(382, 501)
+        MainWindow.resize(501, 532)
+        MainWindow.setMinimumSize(QSize(501, 532))
+        MainWindow.setMaximumSize(QSize(501, 532))
+        icon = QIcon()
+        icon.addFile(u"../../../Downloads/free-icon-login-1674704.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        MainWindow.setWindowIcon(icon)
         MainWindow.setStyleSheet(u"*\n"
 "{\n"
 "	font-family:Comic Sans MS;\n"
 "	font-size:24px;\n"
 "}\n"
 "\n"
-"QFrame\n"
+"#frame\n"
 "{\n"
 "	background:#333;\n"
 "	border-radius:15px;\n"
 "}\n"
 "\n"
+"#frame_2\n"
+"{\n"
+"	background:url(C:\\Users\\Wester35\\Downloads\\sgustok_fraktal_sirenevyj_132619_1600x1200.jpg);\n"
+"	border-radius:10px;\n"
+"}\n"
+"\n"
 "QPushButton\n"
 "{\n"
 "color:white;\n"
-"/*background:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0.545, stop:0 rgba(255, 0, 0, 255), stop:1 rgba(255, 255, 0, 255));   url(:/images/free-icon-login-1674704.png)*/\n"
-"	background:red;\n"
-"	border-radius:15px;\n"
+"background:#4B0082;\n"
+"border-radius:15px;\n"
 "}\n"
 "\n"
 "QPushButton:hover\n"
 "{\n"
-"	color:red;\n"
+"	color:#4B0082;\n"
 "	background:#333;\n"
 "	border-radius:15px;\n"
 "}\n"
 "\n"
 "QToolButton\n"
 "{\n"
-"background:red;\n"
+"background:#4B0082;\n"
 "border-radius:60px;\n"
+"}\n"
+"\n"
+"QMainWindow\n"
+"{\n"
+"background:url('resources/sgustok_fraktal_sirenevyj_132619_1600x1200.jpg');\n"
+"}\n"
+"\n"
+"QLineEdit\n"
+"{\n"
+"background:transparent;\n"
+"border:none;\n"
+"color:#717072;\n"
+"border-bottom:1px solid #717072;\n"
 "}")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setMinimumSize(QSize(501, 532))
+        self.centralwidget.setMaximumSize(QSize(501, 532))
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(10, 80, 361, 361))
+        self.frame.setGeometry(QRect(70, 110, 361, 361))
         self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
         self.label = QLabel(self.frame)
@@ -70,17 +95,30 @@ class Ui_MainWindow(object):
         self.pushButton = QPushButton(self.frame)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(10, 290, 341, 61))
+        self.usernameEdit = QLineEdit(self.frame)
+        self.usernameEdit.setObjectName(u"usernameEdit")
+        self.usernameEdit.setGeometry(QRect(20, 130, 321, 22))
+        self.passwordEdit = QLineEdit(self.frame)
+        self.passwordEdit.setObjectName(u"passwordEdit")
+        self.passwordEdit.setGeometry(QRect(20, 200, 321, 22))
         self.toolButton = QToolButton(self.centralwidget)
         self.toolButton.setObjectName(u"toolButton")
-        self.toolButton.setGeometry(QRect(130, 10, 121, 121))
-        icon = QIcon()
-        icon.addFile(u"resources/free-icon-login-1674704.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.toolButton.setIcon(icon)
+        self.toolButton.setGeometry(QRect(190, 40, 121, 121))
+        icon1 = QIcon()
+        icon1.addFile(u"resources/free-icon-login-1674704.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.toolButton.setIcon(icon1)
         self.toolButton.setIconSize(QSize(78, 78))
+        self.frame_2 = QFrame(self.centralwidget)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setGeometry(QRect(0, 0, 501, 532))
+        self.frame_2.setMinimumSize(QSize(501, 532))
+        self.frame_2.setMaximumSize(QSize(501, 532))
+        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.frame_2.raise_()
+        self.frame.raise_()
+        self.toolButton.raise_()
 
         self.retranslateUi(MainWindow)
 
@@ -91,6 +129,11 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Authorization", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Login here", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Log In", None))
+        self.usernameEdit.setText("")
+        self.usernameEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Username", None))
+        self.passwordEdit.setInputMask("")
+        self.passwordEdit.setText("")
+        self.passwordEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
         self.toolButton.setText("")
     # retranslateUi
 
